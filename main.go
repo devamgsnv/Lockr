3cc7e36befff48018228501dc554a356f90d0355
devamgsnv <amgsnv@yandex.ru>
2025-04-08 10:11:30 +0500

    implemented the division of the project into files

--- main.go
+++ main.go
@@ -1,80 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math/rand/v2"
-	"net/url"
-	"time"
 )
-
-type account struct {
-	login    string
-	password string
-	url      string
-}
-
-type accountWithTimeStamp struct {
-	createdAt time.Time
-	updatedAt time.Time
-	account
-}
-
-func (acc *account) outputPassword() {
-	fmt.Println(acc.login, acc.password, acc.url)
-}
-
-func (acc *account) generatePassword(n int) {
-	res := make([]rune, n)
-	for i := range res {
-		res[i] = letterRunes[rand.IntN(len(letterRunes))]
-	}
-	acc.password = string(res)
-}
-
-func newAccountWithTimeStamp(login, password, urlString string) (*accountWithTimeStamp, error) {
-	if login == "" {
-		return nil, errors.New("INVALID_LOGIN")
-	}
-	_, err := url.ParseRequestURI(urlString)
-	if err != nil {
-		return nil, errors.New("INVALID_URL")
-	}
-	newAcc := &accountWithTimeStamp{
-		createdAt: time.Now(),
-		updatedAt: time.Now(),
-		account: account{
-			url:      urlString,
-			login:    login,
-			password: password,
-		},
-	}
-	if password == "" {
-		newAcc.generatePassword(12)
-	}
-	return newAcc, nil
-}
-
-//func newAccount(login, password, urlString string) (*account, error) {
-//	if login == "" {
-//		return nil, errors.New("INVALID_LOGIN")
-//	}
-//	_, err := url.ParseRequestURI(urlString)
-//	if err != nil {
-//		return nil, errors.New("INVALID_URL")
-//	}
-//	newAcc := &account{
-//		url:      urlString,
-//		login:    login,
-//		password: password,
-//	}
-//	if password == "" {
-//		newAcc.generatePassword(12)
-//	}
-//	return newAcc, nil
-//}
-
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
 
 func main() {
 	login := promptData("Enter login")
